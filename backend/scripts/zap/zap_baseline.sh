3cf03b2cbf942bc9bfbcac1c05e9ea4a23bbd3fd
Kalle Olumets <olumetskalle@gmail.com>
2026-05-23 23:12:33 +0300

    zap #2

--- backend/scripts/zap/zap_baseline.sh
+++ backend/scripts/zap/zap_baseline.sh
@@ -1,8 +1,10 @@
 mkdir -p zap-reports
 
+TS=$(date +"%Y%m%d_%H%M%S")
+
 docker run --rm --network=host \
   -v "$(pwd)/zap-reports:/zap/wrk/:rw" \
   ghcr.io/zaproxy/zaproxy:stable \
   zap-baseline.py \
   -t http://localhost:5173 \
-  -r zap-portal-20260523_223800.html
+  -r "zap-portal-${TS}.html"
